1409c9edf496da262cd16c992ae6c93152ca416c
Hernán Molinari <40526668+pidurffi@users.noreply.github.com>
2019-11-19 15:45:03 -0300

    Update functions.js

--- src/AppBundle/Resources/public/js/functions.js
+++ src/AppBundle/Resources/public/js/functions.js
@@ -61,9 +61,11 @@ $(document).ready(function(){
 			if(value == 0) {
 				$('.gallery ul li').removeClass('active');
 				$('.gallery ul li.one').addClass('active');
+
 			} else if(value == 1) {
 				$('.gallery ul li').removeClass('active');
 				$('.gallery ul li.two').addClass('active');
+                carga("{{ asset('bundles/app/images/panoramicas/suite/') }}");
 			} else if(value == 2) {
 				$('.gallery ul li').removeClass('active');
 				$('.gallery ul li.three').addClass('active');
